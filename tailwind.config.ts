faaf5090eee10de7e0cb0a266824833b50f3d857
25frame <james.j.lefkowitz@gmail.com>
2026-04-29 12:50:35 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-"@tailwindcss/postcss": "latest",
 import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}"],
